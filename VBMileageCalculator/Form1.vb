9ced4a15f89858f0c412d3e877e11f9de99e49f3
William Farmer <willetfarm@yahoo.com>
2020-07-11 22:05:57 +0100

    Set properties; Build; Execute - CompareStep into, Step over, Step out

--- VBMileageCalculator/Form1.vb
+++ VBMileageCalculator/Form1.vb
@@ -1,3 +1,20 @@
 ﻿Public Class Form1
+    Private Sub btnGetMileage_Click(sender As Object, e As EventArgs) Handles btnGetMileage.Click
+        'Call the GetAswer() method
+        GetAnswer()
+        MessageBox.Show("Done")
+    End Sub
 
+    'Create a method to calculate mileage
+    Public Sub GetAnswer()
+
+        'Create three variables to work with
+        Dim Mileage, Distance, Gallons As Decimal
+
+        'Add code to set these values from the UI
+        Distance = txtDistance.Text
+        Gallons = txtGallons.Text
+        Mileage = Distance / Gallons
+        MessageBox.Show(Mileage.ToString, "Answer")
+    End Sub
 End Class
