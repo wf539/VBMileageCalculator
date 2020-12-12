8df5c3734eb7cc86b853b3064a69435e61b9b874
William Farmer <willetfarm@yahoo.com>
2020-12-12 20:36:21 +0000

    Initial commit

---
+++ VBMileageCalculator/MyCustomException.vb
@@ -0,0 +1,5 @@
+﻿Public Class MyCustomException
+    Inherits Exception
+    'This Class demos how to make a simple Custom Exception Class
+    Public Shadows Message As String = "Your friendly message here"
+End Class
