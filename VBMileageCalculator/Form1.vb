cc5b4319309df22cc58feed3f30a0bd29f3ef994
William Farmer <willetfarm@yahoo.com>
2020-12-12 20:38:30 +0000

    Commit ready to migrate project

--- VBMileageCalculator/Form1.vb
+++ VBMileageCalculator/Form1.vb
@@ -11,10 +11,42 @@
         'Create three variables to work with
         Dim Mileage, Distance, Gallons As Decimal
 
-        'Add code to set these values from the UI
-        Distance = txtDistance.Text
-        Gallons = txtGallons.Text
-        Mileage = Distance / Gallons
+        Try 'Add this line to start Try-Catch block
+            'Set these values from the UI
+            Distance = txtDistance.Text
+            Gallons = txtGallons.Text
+
+            'Check for zeroes
+            If Distance = 0 Then
+                'Cause an Exception to be thrown
+                Throw New MyCustomException
+            End If
+
+            'Calculate the Mileage
+            Mileage = Distance / Gallons
+
+        Catch ex As DivideByZeroException
+            'Catch Divide By Zero errors and use the object from
+            'Microsoft's DivideByZeroException Class to show a Message
+            'MessageBox.Show(Mileage.ToString, "Answer")
+            MessageBox.Show(ex.Message)
+
+        Catch ex As MyCustomException
+            'Catch all MyCustomException errors,
+            'Use the Object to show a custom error message
+            MessageBox.Show(ex.Message)
+
+        Catch
+            'Catch any other type of Exception
+            MessageBox.Show("There was an error")
+
+        Finally
+            'Put code here that you always want to run
+            MessageBox.Show("Inside Finally")
+
+        End Try
+
         MessageBox.Show(Mileage.ToString, "Answer")
+
     End Sub
 End Class
